cdc2fdde6e4c9247ebabb3a241c1535d48023ff5
Dignoes Ricart <hugo.dignoes@ubc.ca>
2025-10-31 16:18:52 -0700

    Updating to include West End and Central sensors

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -24,7 +24,9 @@ import pytz
 # EDIT ME
 # ───────────────────────────────────────────────────────────────
 SENSORS_WANTED: set[str] | None = {
-    "2021", "2022","2032", "2040", "2042", "2043", "2024", "2030", "MOD-00632", "MOD-00616", "MOD-00625", "MOD-00631", "MOD-00623"    # set to None ⇒ auto‑discover all sub‑folders
+    "2021", "2022","2032", "2040", "2042", "2043", "2024", "2030", 
+    "MOD-00632", "MOD-00616", "MOD-00625", "MOD-00631", "MOD-00623",
+    "2023", "MOD-00628", "MOD-00628" # set to None ⇒ auto‑discover all sub‑folders
 }
 
 BASE_DIR       = Path("calibrated_data")          # per‑sensor sub‑folders
